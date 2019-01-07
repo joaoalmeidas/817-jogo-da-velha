85cb2118347fe7a9cfff731cf0a6425742010671
joao <joao182cx@gmail.com>
2019-01-07 17:25:39 -0200

    Criação dos getters e setters.

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,4 +1,32 @@
 
 public class JogoDaVelha {
+	
+	private Jogada[][] tabuleiro = new Jogada[3][3];
+
+	public JogoDaVelha() {
+		super();
+		for(int i = 0; i < tabuleiro.length; i++) {
+			
+			for(int j = 0; j < tabuleiro[0].length; j++) {
+				
+				tabuleiro[i][j] = Jogada.EMPTY;
+				
+			}
+			
+		}
+	}
+
+	public Jogada[][] getTabuleiro() {
+		return tabuleiro;
+	}
+
+	public void setTabuleiro(Jogada[][] tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+	
+	
+	
+	
+	
 
 }
