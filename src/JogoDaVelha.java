9841b76b0ac8802ba5330c0c5a13438fb7593e71
joao <joao182cx@gmail.com>
2019-01-07 17:42:55 -0200

    Criação do método exibeTabuleiro().

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -24,7 +24,40 @@ public class JogoDaVelha {
 		this.tabuleiro = tabuleiro;
 	}
 	
-	
+	public void exibeTabuleiro() {
+		
+		for(int i = 0; i < tabuleiro.length; i++) {
+			
+			for(int j = 0; j < tabuleiro[0].length; j++) {
+				
+				if(tabuleiro[i][j] == Jogada.EMPTY) {
+					
+					System.out.printf(" ");
+					
+				}else if(tabuleiro[i][j] == Jogada.O) {
+					
+					System.out.printf("O");
+					
+				}else if(tabuleiro[i][j] == Jogada.X) {
+					
+					System.out.printf("X");
+					
+				}
+				
+				
+				if(j != tabuleiro[0].length - 1) {
+					
+					System.out.printf("|");
+					
+				}
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
 	
 	
 	
---
+++ src/JogoDaVelhaTeste.java
@@ -0,0 +1,12 @@
+
+public class JogoDaVelhaTeste {
+
+	public static void main(String[] args) {
+		
+		JogoDaVelha velha = new JogoDaVelha();
+		
+		velha.exibeTabuleiro();
+
+	}
+
+}
